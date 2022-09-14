dda524909b893acbc4f19e6515d39d43476e638d
Alex <abautistaoax@hotmail.com>
2022-09-14 08:45:47 -0500

    segundo intento de cambio

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo, segundo cambio para aprobación!'));
+app.get('/', (req, res) => res.send('Hola Mundo, second change for approbation!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
